e98ea53d21183701ae10c022dc113e88b69a02ab
Abhirup "Avi" Bhadore <82343305+avibhadore@users.noreply.github.com>
2022-03-31 19:51:23 -0400

    added comments.

--- Grappler.vhd
+++ Grappler.vhd
@@ -15,8 +15,8 @@ END ENTITY grappler;
 	
 ARCHITECTURE grappler_SM OF grappler IS
 	
-TYPE STATE_NAMES IS (D,C,O);
-SIGNAL current_state, next_state : STATE_NAMES; --these signals will hold D,C, or O.
+TYPE STATE_NAMES IS (D,C,O);  -- Disabled state, Closed Grappler State, Open Grappler State respectively.
+SIGNAL current_state, next_state : STATE_NAMES; 
 
 BEGIN
 
@@ -25,9 +25,9 @@ BEGIN
 Register_Section: PROCESS (clk, RESET, next_state) 
 	BEGIN
 	IF (reset = '1') THEN 
-		current_state <= D;
+		current_state <= D; --this sets Disabled as our reset state.
 	ELSIF(rising_edge(clk)) THEN
-		current_state <= next_state; --if not async reset, next state becomes current state
+		current_state <= next_state; --if not async reset, proceed to next state.
 	END IF;
 END PROCESS;
 
@@ -37,28 +37,28 @@ Transition_Section: PROCESS (grappler_en, grappler_in,current_state)
 	BEGIN
 	CASE current_state IS
 		WHEN D =>
-		IF(grappler_en = '1') THEN
+		IF(grappler_en = '1') THEN --enabling grappler means it starts off as closed.
 			next_state <= C;
 		ELSE
 			next_state <= D;
 		END IF;
 		
 		WHEN C =>
-		IF (grappler_en = '0') THEN
+		IF (grappler_en = '0') THEN --turning off grapple enabler means it should be disabled.
 			next_state <= D;
 		ELSIF (grappler_in = '1') THEN
-			next_state <= O;
+			next_state <= O;  --otherwise, actually follow the button push and open the grappler.
 		ELSE
-			next_state <= C;
+			next_state <= C;  --if the button isn't pushed, just idle.
 		END IF;
 		
 		WHEN O =>
-		IF (grappler_en = '0') THEN
-			next_state <= D;
-		ELSIF (grappler_in = '0') THEN
+		IF (grappler_en = '0') THEN --immediately jump to disabled state 
+			next_state <= D; 
+		ELSIF (grappler_in = '0') THEN  --once the press is completed, close the grappler again.
 			next_state <= C;
 		ELSE
-			next_state <= O;
+			next_state <= O; --otherwise, keep the grappler open.
 		END IF;
 
 	END CASE;
@@ -69,13 +69,13 @@ END PROCESS;
 Mealy_SM: PROCESS (current_state)
 BEGIN
 	
-	IF (current_state = D) THEN
+	IF (current_state = D) THEN  --grappler is never open when disabled.
 		grappler_on <= '0';
 		
-	ELSIF (current_state = C) THEN
+	ELSIF (current_state = C) THEN --this state exists specifically for the grappler to be closed, but enabled.
 		grappler_on <= '0';
 		
-	ELSIF (current_state = O) THEN
+	ELSIF (current_state = O) THEN --this state exists specifically for the grappler to be open.
 		grappler_on <= '1';
 		
 	END IF;
@@ -83,6 +83,10 @@ BEGIN
 END PROCESS;
 	
 END;
+	
+	
+	
+	
 	
 	
 	
